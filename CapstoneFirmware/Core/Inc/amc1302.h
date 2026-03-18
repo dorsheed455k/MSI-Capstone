04b707a37623ceed6463c0f87a32d434ea5cf708
Dorsheed <dorshreal@gmail.com>
2026-03-18 00:03:44 -0400

    Fix CurrentSense_t and integrate Web Interface

--- CapstoneFirmware/Core/Inc/amc1302.h
+++ CapstoneFirmware/Core/Inc/amc1302.h
@@ -2,21 +2,22 @@
 #include <stdint.h>
 
 // current_sense.h
-#ifndef CURRENT_SENSE_H
-#define CURRENT_SENSE_H
+#ifndef AMC1302_H
+#define AMC1302_H
 
 typedef struct {
   float iu, iv, iw;
   float offset_u, offset_v, offset_w;
-} CurrentSense_t ;
+} CurrentSense_t;
 
-void CurrentSense_Init(CurrentSense_t  *cs, ADC_HandleTypeDef *hadc);
-void CurrentSense_Start(CurrentSense_t  *cs);
-void CurrentSense_OnDmaComplete(CurrentSense_t  *cs);  // call from ADC DMA callback
+void CurrentSense_Init(CurrentSense_t *cs, ADC_HandleTypeDef *hadc);
+void CurrentSense_Start(CurrentSense_t *cs);
+void CurrentSense_OnDmaComplete(CurrentSense_t *cs); // call from ADC DMA callback
 
-void CurrentSense_CalibrateOffsets(CurrentSense_t  *cs, uint32_t samples);
+void CurrentSense_CalibrateOffsets(CurrentSense_t *cs, uint32_t samples);
 
 /* Set these to your hardware */
-void CurrentSense_SetParams(float vref, uint16_t adc_bits, float amc_gain, float rshunt);
+void CurrentSense_SetParams(float vref, uint16_t adc_bits, float amc_gain,
+                            float rshunt);
 
 #endif
